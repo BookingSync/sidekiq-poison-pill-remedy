52798303894dfbb00ff772053987c23f49be01a5
Karol Galanciak <karol.galanciak@gmail.com>
2024-10-09 11:53:37 +0200

    initial commit

---
+++ lib/sidekiq/poison/pill/remedy.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+require_relative "remedy/version"
+
+module Sidekiq
+  module Poison
+    module Pill
+      module Remedy
+        class Error < StandardError; end
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/sidekiq/poison/pill/remedy/version.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+module Sidekiq
+  module Poison
+    module Pill
+      module Remedy
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
